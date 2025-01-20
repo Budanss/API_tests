3e68461d87ee533265db7a6dace5276051fb3f75
Budanss <Budanovss03@gmail.com>
2025-01-20 18:32:27 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+
+
+URL_SERVICE = "https://710251a5-7f42-4d72-85a5-1c2de1d0120a.serverhub.praktikum-services.ru" # базовый URL веб-сервиса
+DOC_PATH = "/docs/" #к документации веб-сервиса
+LOG_MAIN_PATH = "/api/logs/main/" # логи
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv" # путь к таблице "user_model"
+CREATE_USER_PATH = "/api/v1/users/" # для создания нового пользователя
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/" # поиск продуктов
